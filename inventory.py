7fa5d07b2a6c5d48f074c961ece83622b5456009
Stephen Irons <stephen@brush.co.nz>
2019-05-15 00:42:55 +1200

    Documentation.

--- inventory.py
+++ inventory.py
@@ -1,5 +1,42 @@
+"""
+This file implements the classes for a simple inventory management system.
+
+The idea is that the system keeps a record of 'things'. A thing can be stored in 
+another thing (eg. a cupboard) and that thing can be stored in another thing, and so
+on (eg. a device in a tray in a rack-of-trays on a shelf in a cupboard in a room of
+and office in a building).
+
+From another viewpoint, a thing has slots that other things can plug into to make
+a composite thing. In this case, the composite thing can be stored in a cupboard,
+but the component parts do not their own location; they are part of the composite.
+
+A third viewpoint is a system, where parts of a system are distributed geographically.
+This is not supported.
+
+This system keeps a record of when things change: when an Item is plugged into or removed
+from a slot, when an Item is stored in a container, etc.
+"""
         
 class Item:
+    """
+    An Item is a thing to be kept in the inventory system. It keeps a record of three types 
+    of things:
+
+    * a set of Items that it 'contains'
+
+    * a set of slots that can each be empty or have one Item installed into it, 
+      provided that the Item-installed is of the same Slot_type as the slot
+
+    * set set of key-value parameters, gathered from the extra keyword arguments 
+      provided to the __init__ function. These are useful for location address, manufacturer,
+      model, serial numbers, etc.
+    
+    Each item can be either stored in a container or plugged into a slot, not both. The
+    thinking is that an Item in a slot has no location of its own: it is located in the 
+    same place as the Item whose Slot it is installed in.
+    
+    Installing and removing from slots is managed well; changing containers needs work.
+    """
     def __init__(self, name, format_string=None, fits_into=None, container=None, install_into=None, is_specified_by=None, parameters={}, **kwargs):
         self.name = name
         self.format_string = format_string if format_string is not None else '{name}'
@@ -46,19 +83,22 @@ class Item:
 
         
     def install_into_slot(self, slot, date=None):
-        if self.can_fit_into_slot_type == slot.slot_type:
-            if self._is_installed_in is None:
-                if slot.has_installed is None:
-                    slot.has_installed = self
-                    self._is_installed_in = slot
-                    h = Install_history(slot, self, date, 'Install')
-                    self.change_container(None)
-                else:
-                    Install_history(slot, self, date, 'Slot occupied')
-            else:
-                Install_history(slot, self, date, 'Already installed')
-        else:
+        if self.can_fit_into_slot_type != slot.slot_type:
             Install_history(slot, self, date, 'Does not fit')
+            return
+                        
+        if self._is_installed_in is not None:
+            Install_history(slot, self, date, 'Already installed')
+            return
+        
+        if slot.has_installed is not None:
+            Install_history(slot, self, date, 'Slot occupied')
+            return
+           
+        slot.has_installed = self
+        self._is_installed_in = slot
+        h = Install_history(slot, self, date, 'Install')
+        self.change_container(None)
     
     def remove_from_slot(self, into_container, date=None):
         if self._is_installed_in is not None:
@@ -110,6 +150,10 @@ class Item:
 
 
 class Slot_type:
+    """
+    This could just as well be a string type. But having a class of its own means we
+    things about it properly.
+    """
     def __init__(self, name):
         self.name = name
 
@@ -139,19 +183,6 @@ class Slot:
     def __str__(self):
         return '{} ({})'.format(self.slot_name, self.slot_type.name)
 
-
-class Parameter:
-    def __init__(self, item, name, value):
-        self.item = item
-        self.name = name
-        self.value = value
-        self.item._parameters[name] = self
-
-    def __str__(self):
-        return '{}={}'.format(self.name, self.value)
-
-    def dump(self, level, indent='  '):
-        print('{}{}'.format(indent*level, self))
 
 
 class Install_history:
